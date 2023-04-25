569a12432774befe8274c0af05c46c9afcec5ac8
Dario Ferderber <dario.ferderber@hotmail.com>
2023-04-25 02:21:47 +0200

    refactor: vite replace

--- src/replace.js
+++ src/replace.js
@@ -1,54 +1,48 @@
-(function (factory) {
-  if (typeof module === "object" && typeof module.exports === "object") {
-    var v = factory(require, exports);
-    if (v !== undefined) module.exports = v;
+const execSrcReplacements = (src, replacements) => {
+  for (const replacement of replacements) {
+    if (
+      (typeof replacement.from === 'string' ||
+        replacement.from instanceof RegExp) === false
+    ) {
+      throw new TypeError(
+        "[vite-plugin-replace]: The replacement option 'from' is not of type 'string' or 'RegExp'."
+      )
+    } else if (
+      (typeof replacement.to === 'string' ||
+        replacement.to instanceof Function) === false
+    ) {
+      throw new TypeError(
+        "[vite-plugin-replace]: The replacement option 'to' is not of type 'string' or 'Function'"
+      )
+    } else src = src.replace(replacement.from, replacement.to)
   }
-  else if (typeof define === "function" && define.amd) {
-    define(["require", "exports"], factory);
-  }
-})(function (require, exports) {
-  "use strict";
-  Object.defineProperty(exports, "__esModule", { value: true });
-  exports.replaceCodePlugin = void 0;
-  function execSrcReplacements(src, replacements) {
-    replacements.forEach(function (replacement) {
-      if ((typeof replacement.from === "string" ||
-        replacement.from instanceof RegExp) === false) {
-        throw new Error("[vite-plugin-replace]: The replacement option 'from' is not of type 'string' or 'RegExp'.");
-      }
-      else if ((typeof replacement.to === "string" ||
-        replacement.to instanceof Function) === false) {
-        throw new Error("[vite-plugin-replace]: The replacement option 'to' is not of type 'string' or 'Function'");
-      }
-      else {
-        src = src.replace(replacement.from, replacement.to);
-      }
-    });
-    return src;
-  }
-  function replaceCodePlugin(config) {
-    if (config === undefined) {
-      config = {
-        replacements: [],
-      };
+
+  return src
+}
+
+export const replaceCodePlugin = (config) => {
+  if (config === undefined) {
+    config = {
+      replacements: [],
     }
-    else if ((typeof config === "object" || config !== null) === false) {
-      throw new Error("[vite-plugin-replace]: The configuration is not of type 'Object'.");
-    }
-    else if (Array.isArray(config.replacements) === false) {
-      throw new Error("[vite-plugin-replace]: The configuration option 'replacement' is not of type 'Array'.");
-    }
-    return {
-      name: "transform-file",
-      enforce: 'pre',
-      transform: function (src) {
-        return {
-          code: execSrcReplacements(src, config.replacements),
-          map: null,
-        };
-      },
-    };
+  } else if ((typeof config === 'object' || config !== null) === false) {
+    throw new TypeError(
+      "[vite-plugin-replace]: The configuration is not of type 'Object'."
+    )
+  } else if (Array.isArray(config.replacements) === false) {
+    throw new TypeError(
+      "[vite-plugin-replace]: The configuration option 'replacement' is not of type 'Array'."
+    )
   }
-  exports.replaceCodePlugin = replaceCodePlugin;
-});
-//# sourceMappingURL=index.js.map
+
+  return {
+    name: 'transform-file',
+    enforce: 'pre',
+    transform: function (src) {
+      return {
+        code: execSrcReplacements(src, config.replacements),
+        map: null,
+      }
+    },
+  }
+}
